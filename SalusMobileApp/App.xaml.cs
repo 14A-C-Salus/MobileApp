522e8cc6462c4707fc1a08ef88d99ebb4c075c3c
WittmanBence <wittman.bence@gmail.com>
2023-04-19 16:18:09 +0200

    Removed unused code.

--- SalusMobileApp/App.xaml.cs
+++ SalusMobileApp/App.xaml.cs
@@ -26,6 +26,7 @@ public partial class App : Application
 		{
 			if (_database == null)
 			{
+				// This is a faster method but it doesn't work for some reason. I will try to make it work if everything else is done.
 				//_database = new LocalDatabase(Path.Combine(FileSystem.AppDataDirectory, "SalusMobileApp.db3"));
                 _database = new LocalDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "SalusMobileApp.db3"));
             }
@@ -38,95 +39,5 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new NavigationPage(new SplashScreenPage());
-    }
-
-    protected async override void OnStart()
-    {
-        //database.DeleteLoginData();
-        //database.DeleteEncryptionData();
-        //database.DeleteLocalUserProfile();
-        //OnApplicationStartupCustom();
-        //LoginModel loggedIn = database.GetLoginData();
-        //UserProfileModel userProfile = database.GetLocalUserProfileData();
-        //_userProfile = userProfile;
-        //var login = new LoginPage();
-        //if (loggedIn != null)
-        //{
-        //    tokenExpires = loggedIn.tokenExpires;
-        //    if (ServiceValidation.InternetConnectionValidator())
-        //    {
-        //        if (LoginModel.IsTokenExpired())
-        //        {
-        //            var decryptedPassword = EncryptionModel.DecryptAsync(loggedIn.encryptedPassword);
-        //            await login.CompleteLogin(loggedIn.email, decryptedPassword.Result, false);
-        //        }
-        //        else
-        //        {
-        //            if (userProfile != null)
-        //            {
-        //                var decryptedJwtToken = EncryptionModel.DecryptAsync(database.GetLoginData().jwtToken);
-        //                jwtToken = decryptedJwtToken.Result;
-        //                //await Shell.Current.GoToAsync(nameof(MainMenuPage));
-        //                //await login.NavigateToNextPage(true);
-        //            }
-        //            else
-        //            {
-        //                await Shell.Current.GoToAsync(nameof(ErrorPage));
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        database.OfflineLogin();
-        //    }
-        //}
-    }
-
-    private async void OnApplicationStartupCustom()
-    {
-        LoginModel loggedIn = database.GetLoginData();
-        UserProfileModel userProfile = database.GetLocalUserProfileData();
-        _userProfile = userProfile;
-        var login = new LoginPage();
-        if (loggedIn != null)
-        {
-            tokenExpires = loggedIn.tokenExpires;
-            if (ServiceValidation.InternetConnectionValidator())
-            {
-                if (LoginModel.IsTokenExpired())
-                {
-                    var decryptedPassword = EncryptionModel.DecryptAsync(loggedIn.encryptedPassword);
-                    await login.CompleteLogin(loggedIn.email, decryptedPassword.Result, false);
-                }
-                else
-                {
-                    if (userProfile != null)
-                    {
-                        var decryptedJwtToken = EncryptionModel.DecryptAsync(database.GetLoginData().jwtToken);
-                        jwtToken = decryptedJwtToken.Result;
-                        //await Shell.Current.GoToAsync(nameof(MainMenuPage));
-                        //await login.NavigateToNextPage(true);
-                    }
-                    else
-                    {
-                        await Shell.Current.GoToAsync(nameof(ErrorPage));
-                    }
-                }
-            }
-            else
-            {
-                database.OfflineLogin();
-            }
-        }
-    }
-
-    private void SetMainMenuAsMainpage()
-    {
-        MainPage = new NavigationPage(new MainMenuPage());
-    }
-
-    private void SetLoginSignupAsMainpage()
-    {
-        MainPage = new NavigationPage(new LoginSignupPage());
     }
 }
